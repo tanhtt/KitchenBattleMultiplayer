773b90b2c3ab6f2e9414cb4e8fe3f00f8245e672
tanhtt <flywtan@gmail.com>
2024-09-12 06:50:01 +0700

    sync delivery manager and fix selected visual

--- Assets/Scripts/ResetStaticDataManager.cs
+++ Assets/Scripts/ResetStaticDataManager.cs
@@ -9,6 +9,7 @@ public class ResetStaticDataManager : MonoBehaviour {
         CuttingCounter.ResetStaticData();
         BaseCounter.ResetStaticData();
         TrashCounter.ResetStaticData();
+        Player.ResetStaticData();
     }
 
 }
